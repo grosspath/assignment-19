f67fd07ab779bec34f1001a93d2a405f470f6971
Jerol Graves <jerolgraves@Jerols-MacBook-Pro.local>
2017-04-07 08:25:39 -0400

    second commit

--- src/js/app.js
+++ src/js/app.js
@@ -4,60 +4,69 @@ import { githubApiKey } from '../../secrets.js'
 import $ from 'jquery'
 
 
-let myPersonalInfo = $.getJSON(`https://api.github.com/users/grosspath?access_token=${githubApiKey}`).then(function(serverRes){
+if (window.location.hash === ''){
+  window.location.hash = 'grosspath'
+}
 
+function controllerRouter(){
 
-let myPersonalRepos = $.getJSON(`https://api.github.com/users/grosspath/repos?access_token=${githubApiKey}`).then(function(serverRes2){
+    let currentRoute = window.location.hash.slice(1)
+    let myPersonalInfo = $.getJSON(`https://api.github.com/users/${currentRoute}?access_token=${githubApiKey}`).then(function(serverRes){
+    let myPersonalRepos = $.getJSON(`https://api.github.com/users/${currentRoute}/repos?access_token=${githubApiKey}`).then(function(serverRes2){
 
-let myPersonalInfo = document.querySelector('#app-container')
-myPersonalInfo.innerHTML = `
-<div class="nav-bar">
-  <input type="text" placeholder="Search GitHub"></input>
-  <p class="nav-bar-content">Pull Requests</p>
-  <p class="nav-bar-content">Issues</p>
-  <p class="nav-bar-content">Gist</p>
+    let myPersonalInfo = document.querySelector('#app-container')
+    myPersonalInfo.innerHTML = `
+    <div class="nav-bar">
+      <input type="text" class="searchBarElement" placeholder="Search GitHub" id="searchBar"></input>
+      <p class="nav-bar-content">Pull Requests</p>
+      <p class="nav-bar-content">Issues</p>
+      <p class="nav-bar-content">Gist</p>
+    </div>
+    <div class="overview container">
+      <p class="nav-bar-content">Overview</p>
+      <p class="nav-bar-content">Repositories: ${serverRes.public_repos}</p>
+      <p class="nav-bar-content">Stars</p>
+      <p class="nav-bar-content">Followers: ${serverRes.followers}</p>
+      <p class="nav-bar-content">Following: ${serverRes.following}</p>
+    </div>
+    <div class="body-margin">
+      <img src='${serverRes.avatar_url}'>
+      <h4>${serverRes.name}</h4>
+      <h4>${serverRes.blog}</h4>
+      <h4>${serverRes.location}</h4>
+      <h4>${serverRes.email}</h4>
+      <h4>${serverRes.html_url}</h4>
+      <button>Follow</button>
+      <h6>Block or report user</h6>
+    </div>
+    <div class="side-text">
+    </div>
+    `
 
-</div>
-<div class="overview container">
-  <p class="nav-bar-content">Overview</p>
-  <p class="nav-bar-content">Repositories: ${serverRes.public_repos}</p>
-  <p class="nav-bar-content">Stars</p>
-  <p class="nav-bar-content">Followers: ${serverRes.followers}</p>
-  <p class="nav-bar-content">Following: ${serverRes.following}</p>
-</div>
-<div class="body-margin">
-  <img src='${serverRes.avatar_url}'>
-  <h4>${serverRes.name}</h4>
-  <h4>${serverRes.blog}</h4>
-  <h4>${serverRes.location}</h4>
-  <h4>${serverRes.email}</h4>
-  <h4>${serverRes.html_url}</h4>
-  <button>Follow</button>
-  <h6>Block or report user</h6>
-</div>
-<div class="side-text">
-</div>
-`
+    let repoData = serverRes2.map(function(userObj){
 
-let repoData = serverRes2.map(function(userObj){
+    let repoDataHtml = document.querySelector('.side-text')
 
-let repoDataHtml = document.querySelector('.side-text')
+    repoDataHtml.innerHTML += `
+    <div class="repo-data">
+      <a class="narrow" href="#">${userObj.name}</a>
+      <p class="narrow">${userObj.description}</p>
+      <p class="narrow">${userObj.url}</p>
+    </div>`
+    let searchRepoEl = document.querySelector(".searchBarElement")
 
-repoDataHtml.innerHTML += `
+    searchRepoEl.addEventListener("keypress", function(evt){
+      let repoSearch = ''
+      if (evt.keyCode === 13) {
+        repoSearch = document.getElementById("searchBar").value
+        window.location.hash = repoSearch
+        controllerRouter()
+        document.getElementById("searchBar").value = ''
+      }
+    })
+  })
 
-<div class="repo-data">
-  <a class="narrow" href="#">${userObj.name}</a>
-  <p class="narrow">${userObj.description}</p>
-  <p class="narrow">${userObj.url}</p>
-</div>`
+  })
 })
-// var newInputEl = `grosspath`
-// var pageRouterEl = document.querySelector('#app-container')
-// pageRouterEl.addEventListener('input', function(evt){
-//    newInputEl = evt.target.value
-// })
-// console.log(newInputEl);
-//   return newInputEl
-
-})
-})
+}
+controllerRouter()
